ee246db0e47273da745946f73651d39a72fc8243
RachitAhuja20 <RachitAhuja20@gmail.com>
2017-04-19 20:10:31 +0000

    repaired show and saved article page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,15 +5,23 @@ class ArticlesController < ApplicationController
   end    
   
   def create 
-    #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+   if @article.save
+     flash[:notice] = "Artical was successfully saved."
+     redirect_to article_path(@article)
+   else 
+     render 'new'
+   end
+    
   end
   
+  def show
+    @article = Article.find(params[:id])
+  end
+
   
   private
-  def article_params
-  params.require(:article).permit(:title, :description)
-  end  
+    def article_params
+    params.require(:article).permit(:title, :description)
+    end  
 end
