505aea9276dbe39ab5794ebc9505e502f7de1a23
RachitAhuja20 <RachitAhuja20@gmail.com>
2017-04-22 21:30:20 +0000

    Added destroy and partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,7 +36,13 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+    flash[:notice] = "Article was successfully deleted"
+  end
   
   private
     def article_params
